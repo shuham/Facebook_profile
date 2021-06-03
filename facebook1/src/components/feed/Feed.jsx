fe1567b2e8acfd78fe510d07bb3db6362e40ca87
shuham <32461754+shuham@users.noreply.github.com>
2021-06-03 21:26:48 +0530

    Add files via upload

---
+++ facebook1/src/components/feed/Feed.jsx
@@ -0,0 +1,10 @@
+import "./feed.css"
+
+export default function Feed(){
+     return(
+         <div classname="feed">
+             feed
+         </div>
+     )
+
+}
---
+++ facebook1/src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Topbar from '../../components/topbar/Topbar';
+import "./home.css"
+
+
+export default function Home(){
+
+    return (
+        <>
+        
+        <Topbar/>
+        <div className="homeContainer">
+        <Sidebar/>
+        
+        
+       </div>
+       </>
+    
+
+)
+
+}
